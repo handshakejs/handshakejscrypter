f6b278101bce45faeb3a47840948f0a6acc71023
scottmotte <scott@scottmotte.com>
2014-06-08 20:01:31 -0700

    Initial commit

---
+++ handshakejscrypter_test.go
@@ -0,0 +1,25 @@
+package handshakejscrypter_test
+
+import (
+	"../handshakejscrypter"
+	"log"
+	"testing"
+)
+
+const (
+	ORIGINAL_PLAINTEXT = "some really long plaintext"
+)
+
+func TestEncryptionAndDecryption(t *testing.T) {
+	handshakejscrypter.Setup("supersecret")
+
+	cipher := handshakejscrypter.Encrypt(ORIGINAL_PLAINTEXT)
+	log.Println(cipher)
+
+	plaintext := handshakejscrypter.Decrypt(cipher)
+	log.Println(plaintext)
+
+	if plaintext != ORIGINAL_PLAINTEXT {
+		t.Errorf("Incorrect decrypted plaintext: " + plaintext)
+	}
+}
