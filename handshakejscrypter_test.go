bcfb23b47f21c9fc7139bbd92e0da44a4c1f3fac
scottmotte <scott@scottmotte.com>
2014-06-09 19:16:53 -0700

    Add test to crypter

--- handshakejscrypter_test.go
+++ handshakejscrypter_test.go
@@ -7,11 +7,12 @@ import (
 )
 
 const (
+	KEY                = "somesecretsaltthatis32characters" //32 bytes
 	ORIGINAL_PLAINTEXT = "some really long plaintext"
 )
 
 func TestEncryptionAndDecryption(t *testing.T) {
-	handshakejscrypter.Setup("somesecretsaltofaspecificlength.") //32 bytes
+	handshakejscrypter.Setup(KEY)
 
 	cipher := handshakejscrypter.Encrypt(ORIGINAL_PLAINTEXT)
 	log.Println(cipher)
@@ -20,6 +21,17 @@ func TestEncryptionAndDecryption(t *testing.T) {
 	log.Println(plaintext)
 
 	if plaintext != ORIGINAL_PLAINTEXT {
+		t.Errorf("Incorrect decrypted plaintext: " + plaintext)
+	}
+}
+
+func TestEncryptionAndDecryption2(t *testing.T) {
+	handshakejscrypter.Setup(KEY)
+
+	plaintext := handshakejscrypter.Decrypt("4c916c441a8004057e69183eb11f74bdf1a0c6c5383c774ccf8bd69dd2fd4dba98dc3b6f16afb44a79416d4c")
+	log.Println(plaintext)
+
+	if plaintext != "qt5JjmQWIR3j1pZKQYjA" {
 		t.Errorf("Incorrect decrypted plaintext: " + plaintext)
 	}
 }
